7bff37559ac3547b731ee0d842f3c1e437e8c2a4
José Fernando <166651961+limaamorim@users.noreply.github.com>
2025-06-03 09:35:16 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,16 +1,6 @@
-const mongoose = require('mongoose');
+require('dotenv').config();
 
-const conectarDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-    console.log('MongoDB conectado com sucesso!');
-  } catch (err) {
-    console.error('Erro ao conectar ao MongoDB:', err.message);
-    process.exit(1);
-  }
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+  secretOrKey: process.env.SECRET_OR_KEY
 };
-
-module.exports = conectarDB;
