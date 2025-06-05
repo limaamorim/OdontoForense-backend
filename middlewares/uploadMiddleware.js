4f4c0597e8ac26f1c83d1d6cc5897d203c1e27a7
José Fernando <166651961+limaamorim@users.noreply.github.com>
2025-06-04 22:50:07 -0300

    Update uploadMiddleware.js

--- middlewares/uploadMiddleware.js
+++ middlewares/uploadMiddleware.js
@@ -2,37 +2,15 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 
-// Certifique-se que a pasta uploads existe
 const uploadDir = path.join(__dirname, '../uploads');
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
 }
 
 const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, uploadDir);
-  },
-  filename: function(req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname); // Nome único para o arquivo
-  }
+  destination: (req, file, cb) => cb(null, uploadDir),
+  filename: (req, file, cb) => cb(null, Date.now() + '-' + file.originalname)
 });
 
-const fileFilter = (req, file, cb) => {
-  const filetypes = /jpeg|jpg|png|gif|pdf/;
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  const mimetype = filetypes.test(file.mimetype);
-
-  if (mimetype && extname) {
-    return cb(null, true);
-  } else {
-    cb('Erro: Apenas arquivos de imagem (JPEG, JPG, PNG, GIF) e PDF são permitidos!');
-  }
-};
-
-const upload = multer({
-  storage: storage,
-  limits: { fileSize: 10 * 1024 * 1024 }, // 10MB
-  fileFilter: fileFilter
-});
-
+const upload = multer({ storage });
 module.exports = upload;
