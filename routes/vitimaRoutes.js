28f1dfaa5c40a7c2e48350d3144bf03d23e3ce29
José Fernando <166651961+limaamorim@users.noreply.github.com>
2025-06-12 01:04:42 -0300

    Update vitimaRoutes.js

--- routes/vitimaRoutes.js
+++ routes/vitimaRoutes.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const vitimaController = require('../controllers/vitimaController');
-const upload = require('../middlewares/uploadMiddleware');
 const { autenticarUsuario } = require('../middlewares/authMiddleware');  // IMPORTAÇÃO CORRETA
 
-router.post('/casos/:casoId/vitimas', autenticarUsuario, vitimaController.criarVitima);
-router.get('/casos/:casoId/vitimas', autenticarUsuario, vitimaController.listarVitimasPorCaso);
-router.get('/vitimas/:id', autenticarUsuario, vitimaController.obterVitima);
-router.put('/vitimas/:id', autenticarUsuario, vitimaController.atualizarVitima);
-router.delete('/vitimas/:id', autenticarUsuario, vitimaController.deletarVitima);
+router.get('/', autenticarUsuario, vitimaController.listarTodasVitimas); 
+
+router.post('/:casoId', autenticarUsuario, vitimaController.criarVitima);
+router.get('/:casoId', autenticarUsuario, vitimaController.listarVitimasPorCaso);
+router.get('/detalhes/:id', autenticarUsuario, vitimaController.obterVitima);
+router.put('/detalhes/:id', autenticarUsuario, vitimaController.atualizarVitima);
+router.delete('/detalhes/:id', autenticarUsuario, vitimaController.deletarVitima);
+
 
 module.exports = router;
