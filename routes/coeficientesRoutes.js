3aeca950f144b80b76b2b8a16b1813a8848e6b69
José Fernando <166651961+limaamorim@users.noreply.github.com>
2025-06-17 10:24:47 -0300

    Update coeficientesRoutes.js

--- routes/coeficientesRoutes.js
+++ routes/coeficientesRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 // Endpoint que chama o Flask para pegar os coeficientes
 router.get('/modelo/coeficientes', async (req, res) => {
   try {
-    const response = await axios.get('https://odontoforense-backend.onrender.com/api/modelo/coeficientes');
+    const response = await axios.get('https://odontoforense-flask.onrender.com/api/modelo/coeficientes');
     res.json(response.data);
   } catch (error) {
     console.error('Erro ao buscar coeficientes:', error.message);
